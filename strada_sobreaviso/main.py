c553ec06cbfa1950e4f1ca8cb3f3b9863ee33607
Joe Please Nascimento <joenascimento2000@gmailcom>
2023-07-24 21:09:43 -0300

    refact: remove endpoints and update project
    rotas, controllers, models

--- strada_sobreaviso/main.py
+++ strada_sobreaviso/main.py
@@ -1,106 +1,29 @@
-from typing import Annotated, List
-
-import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
-from sqlalchemy.orm import Session
 
-from strada_sobreaviso.database import config as _database
-from strada_sobreaviso.database import models as _models
+from strada_sobreaviso.rotas import router
 
 app = FastAPI()
-
-_database.Base.metadata.create_all(bind=_database.engine)
-
-
-class ColaboradorBase(BaseModel):
-    name: str
-    squad_id: int
-
-
-class SquadBase(BaseModel):
-    id: int | None = Field(
-        default=None, title='The description of the item', max_length=300
-    )
-    name: str
-
-
-class SquadResponse(BaseModel):
-    id: int | None = Field(
-        default=None, title='The description of the item', max_length=300
-    )
-    name: str
-
-
-def get_db():
-    db = _database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db_deps = Annotated[Session, Depends(get_db)]
+app.include_router(router, prefix='')
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(
     request: Request, exc: RequestValidationError
 ):
-    """Tratando mensagem de erro HTTP 422"""
+    """Tratando mensagem de erro HTTP 400"""
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder(
-            {'detail': 'O parâmetro informado não é um inteiro'}
+            {
+                'status': 'error',
+                'status_code': 400,
+                'message': 'O parâmetro informado não é um inteiro',
+                'detail': RequestValidationError.errors,
+                'method': 'GET',
+                'resource': '/squads/{id}',
+            }
         ),
     )
-
-
-@app.get('/', status_code=status.HTTP_200_OK)
-def health_check():
-    return {'status': 'ok'}
-
-
-@app.get('/health')
-def health_check():
-    return {'status': 'ok'}
-
-
-@app.get('/squads/')
-def get_squads(db: db_deps, skip: int = 0, limit: int = 100):
-    return db.query(_models.Squads).offset(skip).limit(limit).all()
-
-
-@app.get('/squads/{squad_id}')
-def get_squads(squad_id: int, db: db_deps):
-    squad_name = (
-        db.query(_models.Squads).filter(_models.Squads.id == squad_id).first()
-    )
-
-    if not squad_name:
-        raise HTTPException(status_code=404, detail='Squad não existe')
-    return squad_name
-
-
-@app.post(
-    '/squads/create',
-    status_code=status.HTTP_201_CREATED,
-)
-def create_squad(squad: SquadBase, db: db_deps, response_model=SquadResponse):
-    squad_name = _models.Squads(name=squad.name)
-    db.add(squad_name)
-    db.commit()
-    print(f'response_model = {squad_name.id}')
-    return {
-        'result': 'Squad criada com sucesso',
-        'squad': squad.name,
-        'id': squad_name.id,
-    }
-
-
-# if __name__ == '__main__':
-
-#     uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info')
