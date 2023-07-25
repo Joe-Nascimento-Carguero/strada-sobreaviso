e61fc9e7c13fb23c88391d3d8e24274942101368
Joe Please Nascimento <joenascimento2000@gmailcom>
2023-07-25 12:56:30 -0300

    feat: add pydoc

--- strada_sobreaviso/controllers/health_controller.py
+++ strada_sobreaviso/controllers/health_controller.py
@@ -4,5 +4,13 @@ router = APIRouter()
 
 
 @router.get('/')
-async def health_check():
+def health_check() -> dict:
+    """Health Check possibilita verificar o conteúdo de requisições e respostas para checar se um determinado recurso HTTP está funcionando corretamente.
+    Examples:
+        >>> health_check()
+        {'status': 'ok'}
+
+    Returns:
+        dict: Retorna o status_code 200 da api com response status ok
+    """
     return {'status': 'ok'}
